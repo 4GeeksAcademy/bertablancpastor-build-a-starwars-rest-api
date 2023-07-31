8484e7af049d01af7523a65b1fcd068485064743
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-07-31 10:45:23 +0000

    creados modelos y endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,6 +36,8 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+# START endpoints
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +46,191 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#get_all_users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+
+    #consulta a la tabla user
+    user_query = User.query.all()
+    #mapear i serialize toda la tabla de user
+    result = list(map(lambda item: item.serialize(), user_query))
+ 
+    response_body = {
+        "msg": "Get all users",
+        "results": result
+    }
+
+    return jsonify(response_body), 200
+
+# create one user
+@app.route('/users', methods=['POST'])
+def create_user():
+    
+    request_body = request.get_json(force=True)
+    # creacion de un registro en la tabla de user
+    user = User(name=request_body["name"])
+    db.session.add(user)
+    db.session.commit()
+    
+    response_body = {
+        "msg": "user created",
+    }
+    
+    return jsonify(response_body), 200
+
+#get_all_charaters
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    characters_query = Characters.query.all()
+    results = list(map(lambda item: item.serialize(), characters_query))
+
+    print(results)
+
+    response_body = {
+        "msg": "ok characters",
+        "results": results
+    }
+
+    return jsonify(response_body), 200
+
+#get_one_charater
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_one_character(character_id):
+
+    character_query = Characters.query.filter_by(id=character_id).first()
+
+    response_body = {
+        "msg": "ok one character",
+        "result": character_query.serialize()
+    }
+    
+    return jsonify(response_body), 200
+
+# create one character
+@app.route('/characters', methods=['POST'])
+def create_character():
+    
+    request_body = request.get_json(force=True)
+    # creacion de un registro en la tabla de user
+    character = Characters(name=request_body["name"], birth_year=request_body["birth_year"], gender=request_body["gender"], height=request_body["height"], skin_color=request_body["skin_color"], eye_color=request_body["eye_color"])
+    db.session.add(character)
+    db.session.commit()
+    
+    response_body = {
+        "msg": "character created",
+    }
+    
+    return jsonify(response_body), 200
+
+#get_all_planets
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets_query = Planets.query.all()
+    results = list(map(lambda item: item.serialize(), planets_query))
+
+    print(results)
+
+    response_body = {
+        "msg": "ok planets",
+        "results": results
+    }
+
+    return jsonify(response_body), 200
+
+#get_one_planet
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+
+    planet_query = Planets.query.filter_by(id=planet_id).first()
+
+    response_body = {
+        "msg": "ok one planet",
+        "result": planet_query.serialize()
+    }
+    
+    return jsonify(response_body), 200
+
+# create one planet
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    
+    request_body = request.get_json(force=True)
+
+    planet = Planets(name=request_body["name"], climate=request_body["climate"], population=request_body["population"], orbital_period=request_body["orbital_period"], rotation_period=request_body["rotation_period"], diameter=request_body["diameter"])
+    db.session.add(planet)
+    db.session.commit()
+    
+    response_body = {
+        "msg": "planet created",
+    }
+    
+    return jsonify(response_body), 200
+
+#get_all_favorites
+@app.route('/favorites', methods=['GET'])
+def get_all_favorites():
+    favorites_query = Favorites.query.all()
+    results = list(map(lambda item: item.serialize(), favorites_query))
+
+    print(results)
+
+    response_body = {
+        "msg": "ok favorites",
+        "results": results
+    }
+
+    return jsonify(response_body), 200
+
+#get_one_favorite
+@app.route('/favorites/<int:favorite_id>', methods=['GET'])
+def get_one_favorite(favorite_id):
+
+    favorite_query = Favorites.query.filter_by(id=favorite_id).first()
+
+    response_body = {
+        "msg": "ok one favorite",
+        "result": favorite_query.serialize()
+    }
+    
+    return jsonify(response_body), 200
+
+#create_one_favorite
+@app.route('/favorites/', methods=['POST'])
+def create_one_favorite():
+    request_body = request.get_json(force=True)
+    # creacion de un registro en la tabla de user
+    exists = Favorites.query.filter_by(users_id=request_body["users_id"],characters_id=request_body["characters_id"],planets_id=request_body["planets_id"]).first()
+    if exists != None:
+        return jsonify({"msg":"Already exists this favortie"}),400
+    
+    favorite = Favorites(users_id=request_body["users_id"], characters_id=request_body["characters_id"], planets_id=request_body["planets_id"])
+    db.session.add(favorite)
+    db.session.commit()
+    
+    response_body = {
+        "msg": "Favorite created",
+    }
+    
+    return jsonify(response_body), 200
+
+#delete_one_favorite
+@app.route('/favorites/<int:favorite_id>', methods=['DELETE'])
+def delete_one_favorite(favorite_id):
+
+    favorite_delete = Favorites.query.filter_by(id=favorite_id).first()
+
+    db.session.delete(favorite_delete)
+    db.session.commit()
+    
+    response_body = {
+        "msg": "Favorite deleted",
+    }
+    
+    return jsonify(response_body), 200
+
+
+# END endpoints
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
